b0279102d57aefed60a34319e1f4feb4bb4c94b8
arjuna15 <arjunaarasyid62@gmail.com>
2026-06-23 14:25:19 +0700

    UI: Refactor shape divider to use CSS mask-image instead of inline SVG so body grid background shows through transparently

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,14 +6,6 @@ export default function Footer() {
   return (
     <>
       <footer className="travhub-footer">
-        
-        {/* Wavy Shape Divider (TravHub style) */}
-        <div className="footer-shape-divider">
-          <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-              <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="shape-fill"></path>
-          </svg>
-        </div>
-
         <div className="container" style={{ position: 'relative', zIndex: '1', paddingTop: '100px', paddingBottom: '60px' }}>
           <div className="grid" style={{ gridTemplateColumns: 'repeat(auto-fit, minmax(220px, 1fr))', gap: '40px', alignItems: 'start' }}>
             
