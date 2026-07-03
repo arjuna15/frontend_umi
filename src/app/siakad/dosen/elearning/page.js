d2cca25a7615fdd2013149411453856507dedbd8
arjuna15 <arjunaarasyid62@gmail.com>
2026-07-03 22:06:36 +0700

    fix(frontend): import missing ModalShell component in dosen elearning page

--- src/app/siakad/dosen/elearning/page.js
+++ src/app/siakad/dosen/elearning/page.js
@@ -1,6 +1,7 @@
 'use client';
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
+import ModalShell from '../../components/ModalShell';
 
 export default function DosenElearningPage() {
   const router = useRouter();
