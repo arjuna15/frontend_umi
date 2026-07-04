7a49372c11bfcb10162fa5b89d427248e337aa92
arjuna15 <arjunaarasyid62@gmail.com>
2026-07-04 08:56:00 +0700

    Harden Siakad portal target check

--- src/app/siakad/components/portalRoot.js
+++ src/app/siakad/components/portalRoot.js
@@ -2,5 +2,5 @@
 
 export function getPortalRoot() {
   if (typeof document === 'undefined') return null;
-  return document.body instanceof HTMLElement ? document.body : null;
+  return document.body && document.body.nodeType === 1 ? document.body : null;
 }
